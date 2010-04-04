c3d785fabdacd796ceca33a391ac95c2be1ecd51
Wensheng Chen <icydragoon@gmail.com>
2010-04-04 15:30:32 -0400

    Fix reference input and some output

--- Clock.java
+++ Clock.java
@@ -40,16 +40,7 @@ public class Clock {
         System.err.println();
         int memoryCycle = 1;
 
-        System.out.println("References file: " + referenceFile
-                         + "\nPage size: " + pageSize
-                         + "\nVA size: " + vaBits
-                         + "\nPA size: " + paBits
-                         + "\nMiss penalty: " + missPenalty
-                         + "\nDirty page penalty: " + dirtyPagePenalty
-                         + "\nDebug: " + DEBUG
-                         + "\nFrame Count: " + frameCount
-                         + "\n"
-                         + "\nRunning Clock\n========");
+        System.out.println("References file: " + referenceFile + "\nPage size: " + pageSize + "\nVA size: " + vaBits + "\nPA size: " + paBits + "\nMiss penalty: " + missPenalty + "\nDirty page penalty: " + dirtyPagePenalty + "\nDebug: " + DEBUG + "\nFrame Count: " + frameCount + "\n" + "\nRunning Clock\n========");
 
         while (processList.size() > 0) {
             while (!checkIfReady(processList.get(0))) {
@@ -64,17 +55,8 @@ public class Clock {
             processList.remove(0); // Take O(N)
 
             System.out.println("Running " + running.getPid());
-            while (running.topRef()!=null && checkRefIfValid(running.topRef())) {
-                int tmpBurst = running.getBurst();
-                while (tmpBurst > 0) {
-                    --tmpBurst;
-                    for (int i = 0; i < processList.size(); ++i) {
-                        Process tmp = processList.get(i);
-                        tmp.decPenaltyTime(); // nvr goes below 0
-                        processList.set(i, tmp);
-                    }
-                }
-                if(DEBUG){
+            while (running.topRef() != null) {
+                if (DEBUG) {
                     System.out.print("Clock: ");
                     for (int i = 0; i < clockStruct.size(); ++i) {
                         System.out.print(i + " ");
@@ -85,41 +67,57 @@ public class Clock {
                     }
                     System.out.println();
                 }
-                
-                System.out.print("R/W: " + (( running.topRef().getReadOrWrite() )?"R":"W")
-                        + "; VA: " + running.topRef().getAddress()
-                        + "; Page: " + running.topRef().getAddress()/pageSize
-                        + "; Offset: " + running.topRef().getAddress()%pageSize
-                        + "; ");
-                System.out.println();
-                memoryCycle++;
-                running.popRef();
-            }
-            if(running.topRef()==null)break;
-            int freePageIndex = nextFreePage();
-            PageTable.Page tmpPage = pageTable.new Page(running.topRef().getAddress() >> 6);
-            if (freePageIndex >= 0) {
-                clockStruct.add(tmpPage);
-                running.setPenaltyTime(missPenalty);
-            } else {
-                boolean isDirty = false;
-                // this means loop around finding unref'ed. pages.
-                for (int i = 0; i < clockStruct.size(); ++i) {
-                    //check if ref is = 0
-                    if (clockStruct.get(i).referenced == false) {
-                        clockStruct.set(i, tmpPage);
-                        isDirty = true;
-                        running.setPenaltyTime(missPenalty);
-                        break;
-                    }
-                }
-                if(isDirty){
-                      clockStruct.set(0, tmpPage);
-                      running.setPenaltyTime(missPenalty+dirtyPagePenalty);
-                }
-            }
-            running.popRef();// we no longer need this.
 
+                System.out.print("R/W: " + ((running.topRef().getReadOrWrite()) ? "R" : "W") + "; VA: " + running.topRef().getAddress() + "; Page: " + running.topRef().getAddress() / pageSize + "; Offset: " + running.topRef().getAddress() % pageSize + "; ");
+
+                if (checkRefIfValid(running.topRef())) {
+                    int tmpBurst = running.getBurst();
+                    while (tmpBurst > 0) {
+                        --tmpBurst;
+                        for (int i = 0; i < processList.size(); ++i) {
+                            Process tmp = processList.get(i);
+                            tmp.decPenaltyTime(); // nvr goes below 0
+                            processList.set(i, tmp);
+                        }
+                    }
+                    System.out.print("Hit; ");
+                } else {
+                    int freePageIndex = nextFreePage();
+                    PageTable.Page tmpPage = pageTable.new Page(running.topRef().getAddress() >> 6);
+                    if (freePageIndex >= 0) {
+                        clockStruct.add(tmpPage);
+                        running.setPenaltyTime(missPenalty);
+                        System.out.print("Free; ");
+                    } else {
+                        boolean isDirty = false;
+                        // this means loop around finding unref'ed. pages.
+                        for (int i = 0; i < clockStruct.size(); ++i) {
+                            //check if ref is = 0
+                            if (clockStruct.get(i).referenced == false) {
+                                clockStruct.set(i, tmpPage);
+                                isDirty = true;
+                                running.setPenaltyTime(missPenalty);
+                                System.out.print("Clean; ");
+                                break;
+                            }
+                        }
+                        if (isDirty) {
+                            clockStruct.set(0, tmpPage);
+                            running.setPenaltyTime(missPenalty + dirtyPagePenalty);
+                            System.out.print("Dirty; ");
+                        }
+                    }
+
+                }
+                System.out.print("Frame: " + pageTable.getPageAtIndex(running.topRef().getAddress()/pageSize).getFrameNumber()
+                        + "; PA: " );
+
+                System.out.println();
+                running.popRef();// we no longer need this.
+                memoryCycle++;
+            }
+            if (running.topRef() == null)
+                continue;
             // move to the back of the queue
             processList.add(running);
         }
@@ -166,12 +164,13 @@ public class Clock {
                                 "\nburst: " + tmpBurst +
                                 "\nnum of refs: " + tmpNumOfRefs);
                     }
+                    scan.nextLine(); // gobble up the rest of the line
 
                     ArrayList<Reference> refs = new ArrayList<Reference>();
                     while (scan.hasNextLine()) {
                         String line = scan.nextLine();
                         if (line.isEmpty()) {
-                            continue; // this means is an empty line, ignore.
+                            break; 
                         }
                         if (DEBUG_OUT) {
                             System.out.println("\nline: " + line +
@@ -183,7 +182,6 @@ public class Clock {
                                 (scanLine.hasNext("R")) // read or write
                                 );
                         refs.add(reference);
-
                     }
                     processList.add(new Process(tmpPid, tmpBurst, tmpNumOfRefs, refs));
                 }
@@ -251,12 +249,10 @@ public class Clock {
 
     boolean checkRefIfValid(Reference ref) {
         int VA = ref.getAddress();
-        int index = VA /pageSize ;
+        int index = VA / pageSize;
 
-        if(DEBUG_OUT){
-            System.out.println("checking for out of bound index is:"+index
-                    +"\nVirtual Address: "+VA
-                    );
+        if (DEBUG_OUT) {
+            System.out.println("checking for out of bound index is:" + index + "\nVirtual Address: " + VA);
 
         }
         return pageTable.getPageAtIndex(index).valid;
